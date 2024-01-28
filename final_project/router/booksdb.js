22efeeff166e158202466f2122dc493f400ee1cb
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2024-01-28 03:17:17 -0500

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,17 +1,3 @@
-// let books = {
-//       1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-//       2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-//       3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-//       4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-//       5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-//       6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-//       7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-//       8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-//       9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-//       10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
-// }
-
-// module.exports=books;
 let books = {
       1: { "author": "Chinua Achebe", "title": "Things Fall Apart", "isbn": "978-0-435-91000-0", "reviews": {} },
       2: { "author": "Hans Christian Andersen", "title": "Fairy tales", "isbn": "978-1-567-91001-1", "reviews": {} },
